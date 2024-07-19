16135508edf202356022a88e821854e9970e9fb1
Patter1989 <125509804+Patter1989@users.noreply.github.com>
2024-07-19 22:55:12 +0200

    profile+css

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Profile from "./components/Profile/Profile.jsx";
+import userData from "../src/userData.json";
+
+const App = () => {
+	return (
+		<>
+			<Profile
+				name={userData.username}
+				tag={userData.tag}
+				location={userData.location}
+				image={userData.avatar}
+				stats={userData.stats}
+      />
+      
+		</>
+	);
+};
+
+
+
+export default App;
+
+
